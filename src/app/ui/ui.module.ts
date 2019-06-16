a22a4ac15826807c95b5cf17170eb3783712898a
fjgallard <fjdgallardo@gmail.com>
2019-06-16 09:55:42 +0800

    Installed Bootstrap

--- src/app/ui/ui.module.ts
+++ src/app/ui/ui.module.ts
@@ -1,12 +1,15 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { LoginComponent } from './login/login.component';
-import { CoreModule } from '../core/core.module';
+
+// Bootstrap
+import {NgbModule} from '@ng-bootstrap/ng-bootstrap';
 
 @NgModule({
   declarations: [LoginComponent],
   imports: [
-    CommonModule
+    CommonModule,
+    NgbModule
   ],
   exports: []
 })
