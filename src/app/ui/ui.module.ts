e85825d3c78a1ce9237a2a4abc6cc4763ed46b81
fjgallard <fjdgallardo@gmail.com>
2019-06-16 12:15:27 +0800

    Switched from ng-bootstrap to classic bootstrap

--- src/app/ui/ui.module.ts
+++ src/app/ui/ui.module.ts
@@ -2,15 +2,12 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { LoginComponent } from './login/login.component';
 
-// Bootstrap
-import {NgbModule} from '@ng-bootstrap/ng-bootstrap';
 import { NavbarComponent } from './navbar/navbar.component';
 
 @NgModule({
   declarations: [LoginComponent, NavbarComponent],
   imports: [
-    CommonModule,
-    NgbModule
+    CommonModule
   ],
   exports: [NavbarComponent]
 })
