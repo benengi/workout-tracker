e2555f99b2e6bc872a1cf7915fe4b89d485d00e7
fjgallard <fjdgallardo@gmail.com>
2019-06-22 13:13:32 +0800

    Can Edit the Name

--- src/app/ui/program/program-details/program-details.component.ts
+++ src/app/ui/program/program-details/program-details.component.ts
@@ -3,6 +3,8 @@ import { ActivatedRoute } from '@angular/router';
 import { TrainingService } from 'src/app/core/training/training.service';
 import { Observable } from 'rxjs';
 import { Program } from 'src/app/models/training';
+import { FormBuilder, FormGroup } from '@angular/forms';
+import { switchMap } from 'rxjs/operators';
 
 @Component({
   selector: 'app-program-details',
@@ -11,15 +13,43 @@ import { Program } from 'src/app/models/training';
 })
 export class ProgramDetailsComponent implements OnInit {
 
+  program: Program;
   program$: Observable<Program>;
+  programNameForm: FormGroup;
+
+  editProgramMode = false;
 
   constructor(
     public route: ActivatedRoute,
-    private training: TrainingService
+    private training: TrainingService,
+    private fb: FormBuilder
   ) { }
 
   ngOnInit() {
     this.program$ = this.getProgram(this.route.snapshot.paramMap.get('id'));
+    this.program$.subscribe(program => {
+      this.program = program;
+      this.initNameForm(this.program);
+    });
+  }
+
+  saveName() {
+    this.program.displayName = this.programNameForm.value.displayName;
+    this.program = { ...this.program };
+
+    this.training.updateProgram(this.program).subscribe(() => {
+      this.toggleEditNameMode();
+    });
+  }
+
+  toggleEditNameMode() {
+    this.editProgramMode = !this.editProgramMode;
+  }
+
+  initNameForm(program: Program) {
+    this.programNameForm = this.fb.group({
+      displayName: program.displayName
+    });
   }
 
   private getProgram(id: string): Observable<Program> {
